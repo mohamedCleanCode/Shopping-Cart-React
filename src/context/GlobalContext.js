22390511d2bedcdc01e12ab04627af39a8f8e8f4
mohamed <m635379@gmail.com>
2023-02-11 17:25:04 +0200

    fix bug of add to cart with add qantity to item

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -6,8 +6,15 @@ const GlobalContextProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    setCart([...cart, product]);
-    localStorage.setItem("cart", JSON.stringify(cart));
+    const found = cart.find((item) => item.id === product.id);
+    if (found) {
+      console.log(found);
+      found.qantity += 1;
+      localStorage.setItem("cart", JSON.stringify(cart));
+    } else {
+      setCart([...cart, { ...product, qantity: 1 }]);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
   };
 
   useEffect(() => {
